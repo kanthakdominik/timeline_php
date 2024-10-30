cc37be3e9a4bf52f431a5c6d6c4eb3a55c8e208a
kanthakdominik <kanthakdominik@gmail.com>
2024-10-30 15:29:23 +0100

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,21 +1,16 @@
 <?php
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceSchema('https');
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
         }
     }
 }
