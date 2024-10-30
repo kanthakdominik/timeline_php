b029b2619c5bec66a6dc9d2e5513ab1e19ffda0e
kanthakdominik <kanthakdominik@gmail.com>
2024-10-30 14:41:52 +0100

    deploy fixes

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Providers;
+
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(UrlGenerator $url)
+    {
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
+    }
+}
