16b73743eda80bd3c8adcf601666c0682e00cea5
kanthakdominik <kanthakdominik@gmail.com>
2024-10-30 15:12:31 +0100

    wip

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Rejestracja - PGE Narodowy - Wydarzenia 2024</title>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
